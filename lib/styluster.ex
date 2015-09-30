d61a5f142ef635b342a4b1a4f53bb6fe0f15a37c
Martin Stannard <martin@stannard.id.au>
2015-10-01 07:14:01 +1000

    added some enqueue_in method

--- lib/styluster.ex
+++ lib/styluster.ex
@@ -1,2 +1,24 @@
+require Logger
+require Exq
+
 defmodule Styluster do
+
+  def flood(count) do
+    1..count 
+    |> Enum.each(fn(index) -> send(index) end)
+  end
+
+  def send(id) do
+    {:ok, ack} = Exq.enqueue(:exq, "default", "Events", [id]) 
+  end
+
+  def flood_later(count, seconds) do
+    1..count 
+    |> Enum.each(fn(index) -> send_later(index, seconds) end)
+  end
+
+  def send_later(id, seconds) do
+    {:ok, ack} = Exq.enqueue_in(:exq, "default", seconds, "Events", [id]) 
+  end
+
 end
