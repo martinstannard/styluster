2f0f42dc283007b1990f11a9a21f84d1679cac65
Martin Stannard <mstannard@gmail.com>
2015-10-01 14:32:17 +1000

    Spawn db write events

--- lib/events.ex
+++ lib/events.ex
@@ -7,6 +7,10 @@ defmodule Events do
 
   def perform(id) do
     Logger.info "Events perform #{id}"
+    spawn(fn -> insert_event(id) end)
+  end
+
+  def insert_event(id) do
     event = %Event{student_id: id, school_id: id + 1, class_id: id + 2, event_type: "Event::CompleteLesson"}
     Repo.insert!(event)
   end
